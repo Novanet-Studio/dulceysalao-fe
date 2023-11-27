3b7bb9775d4dec2d67bf355d1743084d8ef1ca11
David Arenas <davejs136@gmail.com>
2023-11-27 14:43:04 -0400

    feat: add message when product has not stock

--- components/product/Default.vue
+++ components/product/Default.vue
@@ -10,12 +10,15 @@ const router = useRouter();
 const graphql = useStrapiGraphQL();
 const pruductStore = useProductStore();
 
+const hasStock = computed(() => {
+  return props.product.stock > 0;
+});
+
 async function handleAddToCart() {
   const newProduct = {
     id: props.product.id,
     quantity: 1,
     price: props.product.price,
-    size: '',
   };
 
   cart.addProductToCart(newProduct);
@@ -55,35 +58,34 @@ provide(injectKeys.product, props.product);
     }"
   >
     <template #header>
-      <div class="h-72 w-full relative">
+      <div class="product__item">
         <ProductThumbnailImage />
-        <div
-          class="absolute bg-color-2 bottom-0 left-0 flex justify-center items-center"
-        >
-          <span class="px-4 py-1 font-light" v-if="product.gluten_free">
+        <div class="product__tag">
+          <span class="product__tag-text" v-if="product.gluten_free">
             Sin gluten
           </span>
         </div>
+
+        <span class="product__stock-warning" v-if="!hasStock">
+          No hay inventario
+        </span>
       </div>
     </template>
-    <section class="flex flex-col justify-center gap-4">
-      <h5 class="text-xl text-color-8 font-medium">
+    <section class="product__info-actions">
+      <h5 class="product__title">
         {{ product.name }}
       </h5>
       <p class="text-gray-500">
         {{ product.description }}
       </p>
       <div class="flex gap-2 items-center">
-        <span
-          class="w-[50px] h-[41px] bg-color-2 text-xl flex items-center justify-center rounded-sm"
-        >
-          ${{ product.price }}
-        </span>
+        <span class="product__price"> ${{ product.price }} </span>
         <UButton
           color="color-4"
           icon="i-ph-shopping-cart"
           size="lg"
           label="Agregar"
+          :disabled="!hasStock"
           @click="handleAddToCart"
           :ui="{
             rounded: 'rounded-none',
@@ -93,3 +95,33 @@ provide(injectKeys.product, props.product);
     </section>
   </UCard>
 </template>
+
+<style scoped>
+.product__item {
+  @apply h-72 w-full relative;
+}
+
+.product__tag {
+  @apply absolute bg-color-2 bottom-0 left-0 flex justify-center items-center;
+}
+
+.product__tag-text {
+  @apply px-4 py-1 font-light;
+}
+
+.product__stock-warning {
+  @apply absolute top-0 left-0 w-full h-full bg-black/50 p-4 flex items-center justify-center text-2xl text-color-2;
+}
+
+.product__info-actions {
+  @apply flex flex-col justify-center gap-4;
+}
+
+.product__title {
+  @apply text-xl text-color-8 font-medium;
+}
+
+.product__price {
+  @apply w-[50px] h-[41px] bg-color-2 text-xl flex items-center justify-center rounded-sm;
+}
+</style>
--- types/app.d.ts
+++ types/app.d.ts
@@ -170,7 +170,6 @@ interface CartItem {
   id: string;
   quantity: number;
   price: number;
-  size: string;
 }
 
 interface ShippingInfo {
