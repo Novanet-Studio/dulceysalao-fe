26c8f5aa4c7965c9eac1eb2481e800193157a567
David Arenas <davejs136@gmail.com>
2023-12-05 15:10:33 -0400

    feat: add payment information
    feat: add `zelle` client information
    
    chore: upgrade @nuxt/ui
    
    refactor(zelle): remove copy for holder

--- components/payment/Ves.vue
+++ components/payment/Ves.vue
@@ -4,6 +4,8 @@ const cartStore = useCartStore();
 const { bcvUsd } = await useGetBcvUsd(cartStore.amount);
 const amountRate = computed(() => cartStore.amount * bcvUsd.value);
 
+type AllowedCopyElements = 'phone' | 'rif' | 'amount' | '';
+
 const props = withDefaults(
   defineProps<{ method: 'pago_movil' | 'transferencia' }>(),
   {
@@ -14,6 +16,16 @@ const props = withDefaults(
 const { copied, copy } = useClipboard({
   legacy: true,
 });
+
+const copiedEl = ref<AllowedCopyElements>('');
+
+function setCopiedEl(el: AllowedCopyElements) {
+  copiedEl.value = el;
+
+  setTimeout(() => {
+    copiedEl.value = '';
+  }, 2000);
+}
 
 const formatToVES = (value: number) =>
   new Intl.NumberFormat('es-VE', {
@@ -44,6 +56,120 @@ watch(
 
 <template>
   <div class="mt-6 container">
+    <UCard
+      class="p-6 mb-12 shadow-xl border-none ring-0 ring-transparent"
+      v-if="method === 'pago_movil'"
+    >
+      <div class="grid lg:grid-cols-2 lg:gap-4">
+        <UFormGroup class="mb-4" label="Teléfono">
+          <UInput
+            size="lg"
+            disabled
+            color="gray"
+            model-value="04143213152"
+            :ui="{
+              icon: {
+                trailing: {
+                  pointer: '',
+                },
+              },
+            }"
+          >
+            <template #trailing>
+              <UButton
+                :icon="
+                  copied && copiedEl === 'phone' ? 'i-ph-check' : 'i-ph-copy'
+                "
+                :padded="false"
+                variant="link"
+                :color="copied && copiedEl === 'phone' ? 'green' : 'gray'"
+                @click="
+                  () => {
+                    copy('04143213152');
+                    setCopiedEl('phone');
+                  }
+                "
+              />
+            </template>
+          </UInput>
+        </UFormGroup>
+        <UFormGroup class="mb-4" label="RIF">
+          <UInput
+            size="lg"
+            disabled
+            color="gray"
+            model-value="J-29844392-8"
+            :ui="{
+              icon: {
+                trailing: {
+                  pointer: '',
+                },
+              },
+            }"
+          >
+            <template #trailing>
+              <UButton
+                :icon="
+                  copied && copiedEl === 'rif' ? 'i-ph-check' : 'i-ph-copy'
+                "
+                :padded="false"
+                variant="link"
+                :color="copied && copiedEl === 'rif' ? 'green' : 'gray'"
+                @click="
+                  () => {
+                    copy('298443928');
+                    setCopiedEl('rif');
+                  }
+                "
+              />
+            </template>
+          </UInput>
+        </UFormGroup>
+        <UFormGroup class="mb-4" label="Monto" name="amount">
+          <UInput
+            size="lg"
+            disabled
+            color="gray"
+            v-model="state.amount"
+            :ui="{
+              icon: {
+                trailing: {
+                  pointer: '',
+                },
+              },
+            }"
+          >
+            <template #trailing>
+              <UButton
+                :icon="
+                  copied && copiedEl === 'amount' ? 'i-ph-check' : 'i-ph-copy'
+                "
+                :padded="false"
+                variant="link"
+                color="gray"
+                @click="
+                  () => {
+                    copy(formatToVES(amountRate).replace('Bs.S', '').trim());
+                    setCopiedEl('amount');
+                  }
+                "
+              />
+            </template>
+          </UInput>
+        </UFormGroup>
+        <UFormGroup class="mb-4" label="Banco">
+          <UInput
+            size="lg"
+            disabled
+            color="gray"
+            model-value="Banco venezolano de crédito"
+          />
+        </UFormGroup>
+      </div>
+    </UCard>
+
+    <UDivider label="Reportar pago" />
+
     <UForm :schema="schema" :state="state" @submit="submit">
       <UCard class="p-6 mb-12 shadow-xl border-none ring-0 ring-transparent">
         <div class="grid lg:grid-cols-2 lg:gap-4">
@@ -62,16 +188,31 @@ watch(
             <UInput size="lg" v-model="state.lastName" />
           </UFormGroup>
         </div>
-        <UFormGroup class="mb-4" label="Fecha del pago" name="paymentDate">
+
+        <UFormGroup
+          class="mb-4"
+          label="Fecha del pago"
+          name="paymentDate"
+          v-if="method === 'transferencia'"
+        >
           <UInput size="lg" type="date" v-model="state.date" />
         </UFormGroup>
 
         <div class="grid lg:grid-cols-2 lg:gap-4">
           <UFormGroup
             class="mb-4"
+            label="Fecha del pago"
+            name="paymentDate"
+            v-if="method === 'pago_movil'"
+          >
+            <UInput size="lg" type="date" v-model="state.date" />
+          </UFormGroup>
+          <UFormGroup
+            class="mb-4"
             label="Monto"
             name="amount"
             :help="`La tasa del día BCV es de ${bcvUsd} BsD`"
+            v-if="method === 'transferencia'"
           >
             <UInput
               size="lg"
--- components/payment/Zelle.vue
+++ components/payment/Zelle.vue
@@ -1,7 +1,19 @@
 <script lang="ts" setup>
+type AllowedCopyElements = 'email' | 'amount' | '';
+
+const copiedEl = ref<AllowedCopyElements>('');
+
 const { copied, copy } = useClipboard({
   legacy: true,
 });
+
+function setCopiedEl(el: AllowedCopyElements) {
+  copiedEl.value = el;
+
+  setTimeout(() => {
+    copiedEl.value = '';
+  }, 2000);
+}
 
 const cart = useCartStore();
 const { state, schema, isSending, submit } = usePaymentForm({
@@ -19,6 +31,85 @@ state.amount = '$ ' + cart.amount.toString();
 
 <template>
   <div class="mt-6 container">
+    <UCard class="p-6 mb-12 shadow-xl border-none ring-0 ring-transparent">
+      <div class="grid lg:grid-cols-2 lg:gap-4">
+        <UFormGroup class="mb-4" label="Titular">
+          <UInput
+            size="lg"
+            disabled
+            color="gray"
+            model-value="Graciela Sahmkow"
+          />
+        </UFormGroup>
+        <UFormGroup class="mb-4" label="Correo">
+          <UInput
+            size="lg"
+            disabled
+            color="gray"
+            model-value="gracielasahmkow@gmail.com"
+            :ui="{
+              icon: {
+                trailing: {
+                  pointer: '',
+                },
+              },
+            }"
+          >
+            <template #trailing>
+              <UButton
+                :icon="
+                  copied && copiedEl === 'email' ? 'i-ph-check' : 'i-ph-copy'
+                "
+                :padded="false"
+                variant="link"
+                :color="copied && copiedEl === 'email' ? 'green' : 'gray'"
+                @click="
+                  () => {
+                    copy('gracielasahmkow@gmail.com');
+                    setCopiedEl('email');
+                  }
+                "
+              />
+            </template>
+          </UInput>
+        </UFormGroup>
+        <UFormGroup class="mb-4" label="Monto" name="amount">
+          <UInput
+            size="lg"
+            disabled
+            color="gray"
+            v-model="state.amount"
+            :ui="{
+              icon: {
+                trailing: {
+                  pointer: '',
+                },
+              },
+            }"
+          >
+            <template #trailing>
+              <UButton
+                :icon="
+                  copied && copiedEl === 'amount' ? 'i-ph-check' : 'i-ph-copy'
+                "
+                :padded="false"
+                variant="link"
+                color="gray"
+                @click="
+                  () => {
+                    copy(cart.amount.toString().trim());
+                    setCopiedEl('amount');
+                  }
+                "
+              />
+            </template>
+          </UInput>
+        </UFormGroup>
+      </div>
+    </UCard>
+
+    <UDivider label="Reportar pago" />
+
     <UForm :schema="schema" :state="state" @submit="submit">
       <UCard class="p-6 mb-12 shadow-xl border-none ring-0 ring-transparent">
         <div class="grid lg:grid-cols-2 lg:gap-4">
@@ -37,34 +128,10 @@ state.amount = '$ ' + cart.amount.toString();
             <UInput size="lg" v-model="state.lastName" />
           </UFormGroup>
         </div>
-        <UFormGroup class="mb-4" label="Fecha del pago" name="paymentDate">
-          <UInput size="lg" type="date" v-model="state.date" />
-        </UFormGroup>
 
         <div class="grid lg:grid-cols-2 lg:gap-4">
-          <UFormGroup class="mb-4" label="Monto" name="amount">
-            <UInput
-              size="lg"
-              disabled
-              v-model="state.amount"
-              :ui="{
-                icon: {
-                  trailing: {
-                    pointer: '',
-                  },
-                },
-              }"
-            >
-              <template #trailing>
-                <UButton
-                  :icon="copied ? 'i-ph-check' : 'i-ph-copy'"
-                  :padded="false"
-                  variant="link"
-                  color="gray"
-                  @click="copy(cart.amount.toString().trim())"
-                />
-              </template>
-            </UInput>
+          <UFormGroup class="mb-4" label="Fecha del pago" name="paymentDate">
+            <UInput size="lg" type="date" v-model="state.date" />
           </UFormGroup>
           <UFormGroup
             class="mb-4"
